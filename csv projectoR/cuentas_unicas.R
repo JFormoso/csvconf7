bb3af4f49475d7e875290c2a83b1937687feb4aa
jformoso <jesica.formoso@gmail.com>
2023-03-28 19:37:46 -0300

    Reunión del 28/03

---
+++ csv projectoR/cuentas_unicas.R
@@ -0,0 +1,22 @@
+tb <- readxl::read_xlsx("tablas_nuevas/Tabla_usuarios15.3.xlsx")
+library(tidyverse)
+n_distinct(tb$name)
+
+unicos <- tb %>%
+  filter(!duplicated(name)) 
+
+sum(unicos$statuses_count)
+
+marie <- lookup_users("mariela_rajng")
+
+view(unicos)
+
+unicos2 <- unicos %>%
+  mutate(cuenta_comunidad = "",
+         se_incluye = ifelse(str_detect(str_to_lower(description), "cuenta oficial|^facultad|^ministerio|^universidad|^secretaría|^gobierno|^subsecretaría|^vicerrectoría|^rectoría|^biblioteca |^editorial|^instituto|^laboratorio") |
+                               str_detect(str_to_lower(location), "madrid|españa|spain|segovia|sevilla|granada|barcelona|valéncia|valencia"), 0, 1)) %>%
+  select(id, name, screen_name, location, description, cuenta_comunidad, se_incluye, followers_count, statuses_count, created_at)
+
+writexl::write_xlsx(unicos2, "tablas_nuevas/cuentas.xlsx")
+3976/4  
+  
--- csv projectoR/rtweet.R
+++ csv projectoR/rtweet.R
@@ -24,11 +24,45 @@ for(i in query){
   
 }
 
+id <- lookup_users(users = "1564922322745712640")
+tw <- lookup_tweets(statuses = "1564922322745712640")
+names(tw)
+view(tw)
+names(id)
+id$screen_name
+id$followers_count
+id$created_at
+id$description
 
 tabla_usuarios <- do.call("rbind", u)
 tabla_usuarios
 # writexl::write_xlsx(tabla_usuarios, "tablas_nuevas/Tabla_usuarios15.3.xlsx")
 
 tabla_tweets <- do.call("rbind", t)
-tabla_tweets
+tabla_tweets$retweet_count
+names(tabla_tweets)
+
 # writexl::write_xlsx(tabla_tweets, "tablas_nuevas/Tabla_tweets13.3.xlsx")
+
+
+## Tabla de influencia
+lookup_tweets()
+
+library(rtweet)
+
+# tweet ID for example purposes
+tweet_id <- "139000001234567890"
+
+# get the tweet data
+tweet <- get_tweet(tweet_id)
+
+lookup_tweets(user_id)
+
+
+
+# extract the user ID
+user_id <- tweet$user_id
+
+# print the user ID
+print(user_id)
+
